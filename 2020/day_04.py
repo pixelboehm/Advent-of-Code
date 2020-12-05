7da3e093c526d3ad9d9b209b49918e3773052f59
Maximilian Fischer <maximilian.fischer@uni-due.de>
2020-12-05 21:33:39 +0100

    adds second solution of day 5

--- 2020/day_04.py
+++ 2020/day_04.py
@@ -1,9 +1,12 @@
 """Advent of Code 2020 Day 4: Passport Processing"""
 
+import re
+
 all_lines = []
 required_fields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
 optional_fields = ["cid"]
 answer_one = 0
+answer_two = 0
 
 with open("input_day04.txt") as fp:
     for line in fp.readlines():
@@ -58,8 +61,94 @@ while True:
         break
 
 
-for passport in passport_dict:
+def checkBirthYear(birth_year):
+    return 1920 <= int(birth_year) <= 2002
+
+
+def checkIssueYear(issue_year):
+    return 2010 <= int(issue_year) <= 2020
+
+
+def checkExpirationYear(exp_year):
+    return 2020 <= int(exp_year) <= 2030
+
+
+def checkHeight(height):
+    unit = height[-2:]
+    if unit != "cm" and unit != "in":
+        return False
+    value = int(height[:-2])
+
+    if unit == "cm":
+        return 150 <= value <= 193
+    elif unit == "in":
+        return 59 <= value <= 76
+
+
+def checkHairColor(color):
+    check = re.search(r"^#(?:[0-9a-f]{3}){1,2}$", color)
+    if check != None:
+        return True
+    return False
+
+
+def checkEyeColor(color):
+    if color in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]:
+        return True
+    return False
+
+
+def checkPassportID(value):
+    # return len(identifier) == 9
+    pid_regex = re.compile("[0-9]{9}")
+    if pid_regex.match(value) and len(value) == 9:
+        return True
+    else:
+        return False
+
+
+def checkFields(passport, answer_one, answer_two):
+    key_names = list(passport)
     if checkPassport(passport) == True:
         answer_one += 1
+        current_check = False
+        for entry in key_names:
+            print(entry)
+            if entry == "byr":
+                current_check = checkBirthYear(passport[entry])
 
-print(answer_one)  # Task 1
+            elif entry == "iyr":
+                current_check = checkIssueYear(passport[entry])
+
+            elif entry == "eyr":
+                current_check = checkExpirationYear(passport[entry])
+
+            elif entry == "hgt":
+                current_check = checkHeight(passport[entry])
+
+            elif entry == "hcl":
+                current_check = checkHairColor(passport[entry])
+
+            elif entry == "ecl":
+                current_check = checkEyeColor(passport[entry])
+
+            elif entry == "pid":
+                current_check = checkPassportID(passport[entry])
+
+            elif entry == "cid":
+                continue
+
+            if current_check == False:
+                return answer_one, answer_two
+        answer_two += 1
+        return answer_one, answer_two
+    else:
+        return answer_one, answer_two
+
+
+for passport in passport_dict:
+    answer_one, answer_two = checkFields(passport, answer_one, answer_two)
+
+
+print(answer_one)
+print(answer_two)
