c8e156b02b128806963514ddd7baf834806de27b
Maximilian Fischer <maximilian.fischer@uni-due.de>
2020-12-05 13:17:10 +0100

    refactors solution 1 of day 4

--- 2020/day_04.py
+++ 2020/day_04.py
@@ -9,42 +9,57 @@ with open("input_day04.txt") as fp:
     for line in fp.readlines():
         all_lines.append(line)
 
-current_passport = []
-passports = []
+
+passport_dict = []
+current_passport = dict()
+
+
+def addKeyValuePair(dict, line, current_position):
+    if current_position > 0:
+        line = line[current_position + 1 :]
+    else:
+        line = line[current_position:]
+
+    current_space = line.find(" ")
+
+    if current_space == -1:
+        key = line[: line.find(":")]
+        value = line[line.find(":") + 1 :]
+        value = value.rstrip("\n")
+    else:
+        key = line[: line.find(":")]
+        value = line[line.find(":") + 1 : line.find(" ")]
+        value = value.rstrip("\n")
+
+    dict[key] = value
+
+    if current_space != -1:
+        return addKeyValuePair(dict, line, current_space)
+    return dict
+
+
+def checkPassport(passport):
+    if all(item in passport.keys() for item in required_fields):
+        return True
+
 
 all_lines = iter(all_lines)
 
 while True:
     element = next(all_lines, False)
     if element != False:
-        current_passport.append(element)
+        current_passport = addKeyValuePair(current_passport, element, 0)
         if element == "\n":
-            del current_passport[-1]
-            passports.append(current_passport)
-            current_passport = []
+            current_passport.popitem()
+            passport_dict.append(current_passport)
+            current_passport = {}
     else:
-        passports.append(current_passport)
+        passport_dict.append(current_passport)
         break
 
 
-def findColons(string):
-    return [i for i, char in enumerate(string) if char == ":"]
-
-
-def checkPassport(passport):
-    current_fields = []
-    for entry in passport:
-        colon_list = findColons(entry)
-        for index in colon_list:
-            current_fields.append(entry[index - 3 : index])
-
-    if all(item in current_fields for item in required_fields):
-        return 1
-    return 0
-
-
-for passport in passports:
-    answer_one += checkPassport(passport)
+for passport in passport_dict:
+    if checkPassport(passport) == True:
+        answer_one += 1
 
 print(answer_one)  # Task 1
-
