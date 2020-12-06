d203df52306a2e63ebe9ea1022a4910be31d280d
Maximilian Fischer <maximilian.fischer@uni-due.de>
2020-12-06 17:04:33 +0100

    adds second solution of day 6

--- 2020/day_06.py
+++ 2020/day_06.py
@@ -25,14 +25,52 @@ while True:
 
 def countAnswers(all_groups):
     overall_count = 0
+    answer_two = 0
     for group in all_groups:
+        all_answers = [
+            "a",
+            "b",
+            "c",
+            "d",
+            "e",
+            "f",
+            "g",
+            "h",
+            "i",
+            "j",
+            "k",
+            "l",
+            "m",
+            "n",
+            "o",
+            "p",
+            "q",
+            "r",
+            "s",
+            "t",
+            "u",
+            "v",
+            "w",
+            "x",
+            "y",
+            "z",
+        ]
         group_answers = ""
         for person in group:
             group_answers += person
-        group_count = len(set(group_answers))
+        group_answers = set(group_answers)
+        group_count = len(group_answers)
         overall_count += group_count
-    return overall_count
+        for person in group:
+            person = [char for char in person]
+            all_answers = [value for value in all_answers if value in person]
+        answer_two += len(all_answers)
+
+    return overall_count, answer_two
 
 
-overall_count = countAnswers(all_groups)
+# result.intersection_update(s)
+
+overall_count, answer_two = countAnswers(all_groups)
 print(overall_count)  # answer one
+print(answer_two)  # answer two
