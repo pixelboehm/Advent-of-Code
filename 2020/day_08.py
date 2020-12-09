c029997a1e29abf7fe11b19432236b24ca21fe68
Maximilian Fischer <maximilian.fischer@uni-due.de>
2020-12-09 17:09:07 +0100

    adds second solution of day 8

--- 2020/day_08.py
+++ 2020/day_08.py
@@ -1,35 +1,61 @@
 """Advent of Code 2020 Day 8: Handheld Halting"""
 
 instructions = []
-acc = 0
-visited = []
 
 
 with open("input_day08.txt") as fp:
     for entry in fp.readlines():
         instructions.append(entry.rstrip("\n"))
 
-index = 0
-while index < len(instructions):
-    current_entry = instructions[index]
-    print(current_entry)
-    if index in visited:
-        break
-    else:
+
+def execute(instructions):
+    index = 0
+    acc = 0
+    visited = []
+    while index < len(instructions):
+        terminated = False
+        current_entry = instructions[index]
         visited.append(index)
 
-    if current_entry[:3] == "acc":
-        value = int(current_entry[4:])
-        acc += value
+        if current_entry[:3] == "acc":
+            value = int(current_entry[4:])
+            acc += value
+            index += 1
 
-    if current_entry[:3] == "jmp":
-        value = int(current_entry[4:]) - 1
-        index += value
+        if current_entry[:3] == "jmp":
+            value = int(current_entry[4:])
+            index += value
 
-    if current_entry[:3] == "nop":
-        index += 1
-        continue
+        if current_entry[:3] == "nop":
+            index += 1
+            continue
 
-    index += 1
+        if index in visited:
+            break
+        else:
+            terminated = True
+    return terminated, acc
 
-print(acc)  # answer one
+
+def switchInstruction():
+    for last_index in range(0, len(instructions)):
+        local_instructions = [i for i in instructions]
+        if local_instructions[last_index][:3] == "jmp":
+            local_instructions[last_index] = "nop" + local_instructions[last_index][4:]
+            last_index += 1
+        elif local_instructions[last_index][:3] == "nop":
+            local_instructions[last_index] = "jmp" + local_instructions[last_index][4:]
+            last_index += 1
+        else:
+            last_index += 1
+            continue
+
+        result, value = execute(local_instructions)
+        if result == True:
+            break
+    return value
+
+
+print(execute(instructions))  # answer one
+print(switchInstruction())  # answer two
+
