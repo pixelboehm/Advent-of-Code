65a7fd3cbac0ab2e74b839be39403c8bdfc49243
Maximilian Fischer <maximilian.fischer@uni-due.de>
2020-12-12 13:32:17 +0100

    adds first solution of day 11

---
+++ 2020/day_11.py
@@ -0,0 +1,97 @@
+import copy
+
+with open("input_day11.txt") as f:
+    seats = [list(line) for line in f.read().split()]
+
+
+def printSeats(seats_list):
+    for row in seats_list:
+        print(row)
+
+
+occ_seats = 0
+
+
+def checkNeighbors(y, x):
+    counter = 0
+    x_max = len(seats[0]) - 1
+    y_max = len(seats) - 1
+
+    if x > 0:
+        if seats[y][x - 1] == "#":
+            counter += 1
+        # check left
+
+    if x < x_max:
+        if seats[y][x + 1] == "#":
+            counter += 1
+        # check right
+
+    if y > 0:
+        if seats[y - 1][x] == "#":
+            counter += 1
+        # check top
+
+    if y < y_max:
+        if seats[y + 1][x] == "#":
+            counter += 1
+        # check down
+
+    if x > 0 and y > 0:
+        if seats[y - 1][x - 1] == "#":
+            counter += 1
+        # check top left
+
+    if x > 0 and y < y_max:
+        if seats[y + 1][x - 1] == "#":
+            counter += 1
+        # check down left
+
+    if x < x_max and y > 0:
+        if seats[y - 1][x + 1] == "#":
+            counter += 1
+        # check top right
+
+    if x < x_max and y < y_max:
+        if seats[y + 1][x + 1] == "#":
+            counter += 1
+        # check down right
+
+    return counter
+
+
+def countOccupiedSeats(seats):
+    counter = 0
+    for row in seats:
+        for seat in row:
+            if seat == "#":
+                counter += 1
+    return counter
+
+
+temp_seats = copy.deepcopy(seats)
+seats_changed = 1
+
+while seats_changed != 0:
+    seats_changed = 0
+    for y in range(0, len(seats)):
+        for x in range(0, len(seats[y])):
+
+            if seats[y][x] == ".":
+                continue
+
+            occ_seats += checkNeighbors(y, x)
+
+            if occ_seats == 0 and temp_seats[y][x] != "#":
+                seats_changed += 1
+                temp_seats[y][x] = "#"
+
+            elif occ_seats >= 4 and temp_seats[y][x] != "L":
+                seats_changed += 1
+                temp_seats[y][x] = "L"
+
+            occ_seats = 0
+    seats = copy.deepcopy(temp_seats)
+    print("seats changed:", seats_changed)
+
+print(countOccupiedSeats(seats))
