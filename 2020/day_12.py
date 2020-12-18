f2c478b6bff01a9fa5662937e82aaac30c887739
Maximilian Fischer <maximilian.fischer@uni-due.de>
2020-12-18 16:57:17 +0100

    adds second solution of day 12

--- 2020/day_12.py
+++ 2020/day_12.py
@@ -2,6 +2,7 @@
 
 instructions = []
 answer_one = 0
+answer_two = 0
 
 values = dict()
 values["E"] = 0
@@ -47,36 +48,94 @@ def turnRight(current_heading, current_degree, degree):
     return current_heading, current_degree
 
 
-current_heading = "E"
-current_degree = 90
+def taskOne():
+    current_heading = "E"
+    current_degree = 90
 
-for instruction in instructions:
-    value_list = instruction[1:]
-    value = list(map(int, value_list))[0]
-    if instruction[0] == "F":
-        values[current_heading] += value
+    for instruction in instructions:
+        value_list = instruction[1:]
+        value = list(map(int, value_list))[0]
+        if instruction[0] == "F":
+            values[current_heading] += value
 
-    if instruction[0] == "E":
-        values["E"] += value
+        if instruction[0] == "E":
+            values["E"] += value
 
-    if instruction[0] == "W":
-        values["W"] += value
+        if instruction[0] == "W":
+            values["W"] += value
 
-    if instruction[0] == "N":
-        values["N"] += value
+        if instruction[0] == "N":
+            values["N"] += value
 
-    if instruction[0] == "S":
-        values["S"] += value
+        if instruction[0] == "S":
+            values["S"] += value
 
-    if instruction[0] == "L":
-        current_heading, current_degree = turnLeft(
-            current_heading, current_degree, value
-        )
+        if instruction[0] == "L":
+            current_heading, current_degree = turnLeft(
+                current_heading, current_degree, value
+            )
 
-    if instruction[0] == "R":
-        current_heading, current_degree = turnRight(
-            current_heading, current_degree, value
-        )
+        if instruction[0] == "R":
+            current_heading, current_degree = turnRight(
+                current_heading, current_degree, value
+            )
 
-answer_one = abs(values["E"] - values["W"]) + abs(values["N"] - values["S"])
-print(answer_one)
+    answer_one = abs(values["E"] - values["W"]) + abs(values["N"] - values["S"])
+    print(answer_one)
+
+
+waypoint = (10, 1)
+ship = (0, 0)
+
+
+def changeLocation(ship, value):
+    return (value * ship[0], value * ship[1])
+
+
+def taskTwo():
+    for instruction in instructions:
+        print(instruction)
+        value_list = instruction[1:]
+        value = list(map(int, value_list))[0]
+        if instruction[0] == "F":
+            values = changeLocation(waypoint, value)
+            ship = tuple(map(lambda i, j: i + j, ship, values))
+
+        if instruction[0] == "E":
+            waypoint = (waypoint[0] + value, waypoint[1])
+
+        if instruction[0] == "W":
+            waypoint = (waypoint[0] - value, waypoint[1])
+
+        if instruction[0] == "N":
+            waypoint = (waypoint[0], waypoint[1] + value)
+
+        if instruction[0] == "S":
+            waypoint = (waypoint[0], waypoint[1] - value)
+
+        if instruction[0] == "L":
+            temp = waypoint[0]
+
+            if value == 90:
+                waypoint = (-waypoint[1], temp)
+
+            if value == 180:
+                waypoint = (-waypoint[0], -waypoint[1])
+
+            if value == 270:
+                waypoint = (waypoint[1], -temp)
+
+        if instruction[0] == "R":
+            temp = waypoint[0]
+
+            if value == 90:
+                waypoint = (waypoint[1], -temp)
+            if value == 180:
+                waypoint = (-waypoint[0], -waypoint[1])
+            if value == 270:
+                waypoint = (-waypoint[1], temp)
+
+    print(ship)
+    answer_two = abs(ship[0]) + abs(ship[1])
+    print(answer_two)
+
