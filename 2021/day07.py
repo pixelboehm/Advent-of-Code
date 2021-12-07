f8648741b37c4c2cbc2a66cc663478baa5d8e3b5
Maximilian Fischer <maximilian.fischer@uni-due.de>
2021-12-07 16:59:44 +0100

    adds second solution of day 7

--- 2021/day07.py
+++ 2021/day07.py
@@ -1,10 +1,21 @@
-import numpy as np
+import math
 
 position = open("input_day07.txt").read().split(',')
 position = [int(x) for x in position]
 
 position.sort()
 mid = len(position) // 2
-res = int((position[mid] + position[~mid]) / 2)
+median = int((position[mid] + position[~mid]) / 2)
 
-result_task_one = sum(map(lambda x: abs(x-2), position))
+result_task_one = sum(map(lambda x: abs(x - median), position))
+print("Result Task One: {}".format(result_task_one))
+
+average = math.floor(sum(position) / len(position))
+
+new_distances = list(map(lambda x: abs(x - average), position))
+
+def calculate_fuel(number):
+	return (number ** 2 + number) // 2
+
+result_task_two = sum(map(calculate_fuel, new_distances))
+print("Result Task Two: {}".format(result_task_two))
