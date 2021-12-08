1769e77beb208f418bbaf00ee287618d7f31ec8b
Maximilian Fischer <maximilian.fischer@uni-due.de>
2021-12-08 10:35:09 +0100

    slightly cleaner solution for day 8 part 1

--- 2021/day08.py
+++ 2021/day08.py
@@ -1,12 +1,14 @@
 segments = open("input_day08.txt").read().splitlines()
 segments = [x.split(" | ") for x in segments]
-result_task_one = 0
-unique_signal_segments = [2, 3, 4, 7]
 
-for signal in segments:
-	current_segments = signal[1].split()
-	for segment in current_segments:
-		if(len(segment) in unique_signal_segments):
-			result_task_one += 1
+def part_1(segments):
+	result = 0
+	unique_signal_segments = [2, 3, 4, 7]
+	for signal in segments:
+		current_segments = signal[1].split()
+		for segment in current_segments:
+			if(len(segment) in unique_signal_segments):
+				result += 1
+	return result
 
-print("Result Task One: {}".format(result_task_one))
+print("Result Task One: {}".format(part_1(segments)))
