101ab8257b5097d65eeeab3a50d0163e1bd5e42d
Maximilian Fischer <maximilian.fischer@uni-due.de>
2021-12-13 19:24:50 +0100

    solution for part 2 of day 8 2021

--- 2021/day08.py
+++ 2021/day08.py
@@ -1,5 +1,7 @@
-segments = open("input_day08.txt").read().splitlines()
-segments = [x.split(" | ") for x in segments]
+import itertools
+
+inp = open("input_day08.txt").read().splitlines()
+segments = [x.split(" | ") for x in inp]
 
 def part_1(segments):
 	result = 0
@@ -11,4 +13,40 @@ def part_1(segments):
 				result += 1
 	return result
 
+
+def part_2(inp):
+	result = 0
+	for x,y in [x.split('|') for x in inp]: 
+		l = {len(s): set(s) for s in x.split()}
+		
+		number = ""
+		for z in y.split(" "):
+			if len(z) == 2:
+				number += "1"
+			elif len(z) == 3:
+				number += "7"
+			elif len(z) == 4:
+				number += "4"
+			elif len(z) == 5:
+				z = set(z)
+				if l[3].issubset(z):
+					number += "3"
+				elif len(z.intersection(l[4])) == 3:
+					number += "5"
+				else:
+					number += "2"
+			elif len(z) == 6:
+				z = set(z)
+				if l[4].issubset(z):
+					number += "9"
+				elif l[3].issubset(z):
+					number += "0"
+				else:
+					number += "6"
+			elif len(z) == 7:
+				number += "8"
+		result += int(number)
+	return result
+
 print("Result Task One: {}".format(part_1(segments)))
+print("Result Task Two: {}".format(part_2(inp)))
